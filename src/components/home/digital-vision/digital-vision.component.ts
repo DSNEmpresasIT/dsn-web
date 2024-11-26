1e68351074ebd29aad7ee0fac520d5e85eac2721
DanielMino19 <danielmino926@gmail.com>
2024-11-25 21:28:59 -0300

    start digital-vision

---
+++ src/components/home/digital-vision/digital-vision.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-digital-vision',
+  imports: [],
+  templateUrl: './digital-vision.component.html',
+  styleUrl: './digital-vision.component.css'
+})
+export class DigitalVisionComponent {
+
+}
