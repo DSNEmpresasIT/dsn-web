ddce3a9e6258c7b4682712340bfb1534d317d4a9
DanielMino19 <danielmino926@gmail.com>
2024-11-25 23:45:46 -0300

    responsive and layout digital-vision component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'prefix'
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('../components/home/digital-vision/digital-vision.component').then(c => c.DigitalVisionComponent),
+    title: 'Inicio-DSNEmpresas',
+  },
+];
+
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { DigitalVisionComponent } from "../../components/home/digital-vision/digital-vision.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [DigitalVisionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
