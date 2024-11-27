a0461015985883139dc545abd21ac6916b7f6e3a
iarmolla <ivan.armolla@gmail.com>
2024-11-26 22:02:40 -0300

    feat: add network component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { DigitalVisionComponent } from "../../components/home/digital-vision/digital-vision.component";
 import { MarketingComponent } from "../../components/home/marketing/marketing.component";
+import { NetworkComponent } from "../../components/home/network/network.component";
 
 @Component({
   selector: 'app-home',
-  imports: [DigitalVisionComponent, MarketingComponent],
+  imports: [DigitalVisionComponent, MarketingComponent, NetworkComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
