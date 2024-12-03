2ce96ae3a753b5dc182ab6efaacb8a6e0d43b7d3
iarmolla <ivan.armolla@gmail.com>
2024-12-03 18:54:39 -0300

    add blur

--- src/components/home/brand/brand.component.ts
+++ src/components/home/brand/brand.component.ts
@@ -11,18 +11,22 @@ export class BrandComponent {
     {
       image: "./images/felix_150x150.png",
       alt: "felix",
+      url: 'https://felixmenendez.com.ar'
     },
     {
       image: "./images/aluplast_150x150.png",
       alt: "aluplast",
+      url: 'https://aluplast.com.ar'
     },
     {
       image: "./images/grupoimagenes_150x150.png",
       alt: "grupo imágenes médicas",
+      url: 'https://www.grupoimagenesmedicas.com.ar'
     },
     {
       image: "./images/panorama_150x150.png",
       alt: "panorama",
+      url: 'https://panorama21.com.ar'
     },
   ];
 }
