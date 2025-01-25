a1fd4d239a28ac383788542cb5a4dd99b7a58860
DanielMino19 <danielmino926@gmail.com>
2025-01-25 19:38:49 -0300

    add links in footer

--- src/components/common/footer/footer.component.ts
+++ src/components/common/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CONTACT_INFO } from '../../../utils/enums';
 
 @Component({
   selector: 'app-footer',
@@ -7,24 +8,25 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+  CONTACT_INFO: any = CONTACT_INFO;
   socialMediaItems:any[] = [
     {
-      link:'',
+      link:'https://www.linkedin.com/in/dsnempresas/',
       icon:'./images/blanco_lkdn.png',
       name: 'Linkedin',
     },
     {
-      link:'',
+      link:'https://www.facebook.com/dsnempresas',
       icon:'./images/blanco_fcb.png',
       name: 'Facebook',
     },
     {
-      link:'',
+      link:`https://api.whatsapp.com/send?phone=${CONTACT_INFO.WHATSAPP}`,
       icon:'./images/blanco_ws.png',
       name: 'Whatsapp',
     },
     {
-      link:'',
+      link:'https://www.instagram.com/dsnempresas',
       icon:'./images/blanco_ig.png',
       name: 'Instagram',
     },
